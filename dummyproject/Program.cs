acfcc881c9994e7c05f43aa0c8430495c401db8e
anushkachouhan029 <anushkachouhan029>
2023-01-05 10:00:04 +0530

    [anushka] added uc1sampleproject

---
+++ dummyproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dummyproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
